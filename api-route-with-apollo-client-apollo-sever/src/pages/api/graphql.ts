850fddef3f87015486091725639a3cad60697d81
ikekiyo <ike.kyo0802@gmail.com>
2023-01-09 16:15:50 +0900

    apollo server sample

---
+++ api-route-with-apollo-client-apollo-sever/src/pages/api/graphql.ts
@@ -0,0 +1,38 @@
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { gql } from "graphql-tag";
+
+export const typeDefs = `#graphql
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+export default startServerAndCreateNextHandler(server);
